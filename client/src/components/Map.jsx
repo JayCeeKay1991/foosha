ead7543aa326b43a8161975fb7930cbc8e8a176d
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-04 12:35:13 +0100

    setup corrected

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { useMainContext } from './Context';
+import { defaultLocation } from '../services/mapApiService';
+
+const mapsApiKey = import.meta.env.VITE_MAPS_API_KEY;
+console.log(mapsApiKey, typeof mapsApiKey);
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const defaultCenter = defaultLocation;
+
+function Map({mapAsInput, onLocationSelect, zoom}) {
+  const { location, list } = useMainContext();
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: mapsApiKey
+  });
+
+  const [marker, setMarker] = useState(null);
+  const [mapCenter, setMapCenter] = useState(defaultCenter);
+
+
+  // set the map center to the current location
+  useEffect(() => setMapCenter(location),[location]);
+
+  // choose a location by clicking on the map
+  const onMapClick = mapAsInput ? (e) => {
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    setMarker({ lat, lng });
+    onLocationSelect({ lat, lng });
+  } : null;
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={mapCenter}
+      zoom={zoom}
+      onClick={onMapClick}
+    >
+      {/* if we use the map as an input form */}
+      {mapAsInput && marker && (
+        <Marker position={{ lat: marker.lat, lng: marker.lng }} />
+      )}
+      {/* if we use the map to render items */}
+      {!mapAsInput && list && list.map(elem => (
+        <Marker key={elem._id} position={{ lat: elem.location.coordinates[1], lng: elem.location.coordinates[0] }} />
+      ))}
+    </GoogleMap>
+  ) : <></>;
+
+}
+
+export default Map;
---
+++ client/src/services/mapApiService.js
@@ -0,0 +1,42 @@
+const mapsApiKey = import.meta.env.VITE_MAPS_API_KEY;
+
+export const defaultLocation = {
+  lat: 52.507389,
+  lng: 13.378096
+}
+
+export function fetchUserLocation(setLocation) {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ lat: latitude, lng: longitude });
+      },
+      (error) => {
+        // if location can't be found
+        console.error("Error getting location: ", error);
+        setLocation(defaultLocation); // Berlin as a fallback
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+    // if geolocation is not supported ...
+    setLocation(defaultLocation);
+  }
+}
+
+export async function formatLocation (lat, lng) {
+  const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${mapsApiKey}`;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.results.length > 0) return data.results[0].formatted_address;
+    else {
+      console.error("No results found");
+      return 'No location found';
+    }
+  } catch (error) {
+    console.error("Geocode error:", error);
+    return 'No location found';
+  }
+}

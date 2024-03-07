2a838bd972a046a8f0c35b1010f270e4826c1079
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-07 11:27:43 +0100

    feat: cors headers

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ const cors = require('cors');
 console.log(config);
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+})
 
 app.use(express.json());
 

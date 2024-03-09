352ad259105cf420fd25086976d810657af5cc66
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-09 15:47:06 +0100

    fix: deploy config

--- server/index.js
+++ server/index.js
@@ -5,23 +5,7 @@ const router = require('./router');
 const cors = require('cors');
 
 
-app.use(cors({
-  origin: '*'
-}));
-
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PUT, DELETE'
-//   );
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   next();
-// })
+app.use(cors());
 
 app.use(express.json());
 

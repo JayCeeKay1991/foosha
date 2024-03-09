7fef90e1adc32c17405979b8c5a47bab4c8b8918
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-09 14:42:36 +0100

    fix: cors config

--- server/index.js
+++ server/index.js
@@ -4,21 +4,24 @@ const config = require('./config');
 const router = require('./router');
 const cors = require('cors');
 
-console.log(config);
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-})
+app.use(cors({
+  origin: 'https://foosha.netlify.app/'
+}));
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, DELETE'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   next();
+// })
 
 app.use(express.json());
 
